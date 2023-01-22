f28c2913de8612821c47405c397a417279f2f2d5
Richard Van Camp <richard.van.camp@wolt.com>
2023-01-22 18:03:02 +0200

    Feat: initial commit with dropbox app

---
+++ src/main/kotlin/dev/vancamp/CapstoneApp.kt
@@ -0,0 +1,26 @@
+package dev.vancamp
+
+import org.http4k.client.Java8HttpClient
+import org.http4k.cloudnative.env.Environment
+import org.http4k.cloudnative.env.EnvironmentKey
+import org.http4k.core.Credentials
+import org.http4k.core.then
+import org.http4k.filter.DebuggingFilters.PrintRequest
+import org.http4k.filter.ServerFilters
+import org.http4k.lens.int
+import org.http4k.server.Jetty
+import org.http4k.server.asServer
+
+val BASIC_AUTH_CREDENTIALS = EnvironmentKey.map(String::toCredentials).required("BASIC_AUTH_CREDENTIALS")
+val HTTP_PORT = EnvironmentKey.int().defaulted("HTTP_PORT", 8000)
+
+fun main() {
+    val env = Environment.fromResource("capstone.properties")
+    println(BASIC_AUTH_CREDENTIALS(env))
+    PrintRequest()
+        .then(ServerFilters.BasicAuth("capstone", BASIC_AUTH_CREDENTIALS(env)))
+        .then(Dropbox(env, Java8HttpClient()))
+        .asServer(Jetty(HTTP_PORT(env))).start().block()
+}
+
+private fun String.toCredentials() = split(":").run { Credentials(get(0), get(1)) }
---
+++ src/test/kotlin/dev/vancamp/TestSettings.kt
@@ -0,0 +1,14 @@
+package dev.vancamp
+
+import dev.vancamp.Settings.AWS_BUCKET
+import dev.vancamp.Settings.AWS_CREDENTIALS
+import dev.vancamp.Settings.S3_CREDENTIAL_SCOPE
+import org.http4k.aws.AwsCredentialScope
+import org.http4k.aws.AwsCredentials
+import org.http4k.cloudnative.env.Environment
+
+val TestSettings = Environment.defaults(
+    AWS_CREDENTIALS of AwsCredentials("key", "secretKey"),
+    AWS_BUCKET of "mybucket",
+    S3_CREDENTIAL_SCOPE of AwsCredentialScope("us-east-1", "s3")
+)
